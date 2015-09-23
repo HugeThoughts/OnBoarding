8bba49b42669162c715ee271c64f530403dedfa6
Ashik Nesin <mail@ashiknesin.com>
2015-09-23 16:47:54 +0530

    Changed GitHub form ribben color to green & js optimization

--- js/main.js
+++ js/main.js
@@ -1,22 +1,41 @@
-$('input.text-box').floatlabel({
-    labelClass: 'label'
-});
 
-function isNumberKey(evt) {
-    var charCode = (evt.which) ? evt.which : event.keyCode
-    if (charCode > 31 && (charCode < 48 || charCode > 57))
+// create as many regular expressions here as you need:
+var digitsOnly = /[1234567890]/g;
+var floatOnly = /[0-9\.]/g;
+var alphaOnly = /[A-Za-z]/g;
+var AlphaNumericOnly = /[A-Za-z0-9\.]/g;
+
+function restrictCharacters(myfield, e, restrictionType) {
+    if (!e) var e = window.event
+    if (e.keyCode) code = e.keyCode;
+    else if (e.which) code = e.which;
+    var character = String.fromCharCode(code);
+    // if they pressed esc... remove focus from field...
+    if (code == 27) {
+        this.blur();
         return false;
-
-    if (entry_2039445497.value.length > 9)
-        return false;
-
-    return true;
-};
+    }
+    // ignore if they are press other keys
+    // strange because code: 39 is the down key AND ' key...
+    // and DEL also equals .
+    if (!e.ctrlKey && code != 9 && code != 8 && code != 36 && code != 37 && code != 38 && (code != 39 || (code == 39 && character == "'")) && code != 40) {
+        if (character.match(restrictionType)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 
 /*
     By Osvaldas Valutis, www.osvaldas.info
     Available for use under the MIT License
 */
+
+$('input.text-box').floatlabel({
+    labelClass: 'label'
+});
+
 
 'use strict';
 
